3e172f2840f4c59f1302d0ebd25e86a4527c01dc
jimting <handsome841206@gmail.com>
2017-03-26 16:32:14 +0800

    連線篇教學檔案

---
+++ 連線篇/students.php
@@ -0,0 +1,19 @@
+﻿<html>
+	<head>
+		<title>TestStudent</title>
+	</head>
+	<body>
+		<?php
+			include("connect.php");
+			
+			$sql = "SELECT * FROM students";
+			if($stmt = $db->query($sql))
+			{
+				while($result = mysqli_fetch_object($stmt))
+				{
+					echo '<p>名字： '.$result->name.'，學號：'.$result->number.'</p>';
+				}
+			}
+		?>
+	</body>
+</html>
